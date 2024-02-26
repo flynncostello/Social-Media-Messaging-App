c4d846f3fe0dff7d14340e3e9412149d1cb8a5a1
Flynn Costello <flynn.p.costello@outlook.com>
2024-02-26 19:38:21 +1100

    quick push

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,2 +1,2 @@
 //export const API_ENDPOINT = "http://localhost:5001/api";
-export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || "/api";
+export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || "api";
