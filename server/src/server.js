f6729bf5bf976a120072b0eb171a1116bda314ce
Flynn Costello <flynn.p.costello@outlook.com>
2024-02-27 22:49:10 +1100

    quick push

--- server/src/server.js
+++ server/src/server.js
@@ -11,15 +11,17 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("https://full-stack-to-do-list-app.onrender.com/api/tasks", tasksRouter);
 app.use("/api/tasks", tasksRouter);
 
 
+/*
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'client', 'public', 'index.html'));
 });
+*/
 
 
 //const host = process.env.HOST || '0.0.0.0';
