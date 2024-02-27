fa535e47fca9a24568e4d67c49b8e64d4113b634
Flynn Costello <flynn.p.costello@outlook.com>
2024-02-27 23:06:36 +1100

    changed server code

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
-const path = require("path");
+//const path = require("path");
 
 const tasksRouter = require("./routes/tasksRouter");
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("https://full-stack-to-do-list-app.onrender.com/api/tasks", tasksRouter);
 app.use("/api/tasks", tasksRouter);
