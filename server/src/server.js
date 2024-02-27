0151e5b753deceb3a64d6c1720b7d8f99d4e0581
Flynn Costello <flynn.p.costello@outlook.com>
2024-02-27 21:40:18 +1100

    minor changes to server and api routes

--- server/src/server.js
+++ server/src/server.js
@@ -13,26 +13,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("https://full-stack-to-do-list-app.onrender.com/api/tasks", tasksRouter);
+//app.use("https://full-stack-to-do-list-app.onrender.com/api/tasks", tasksRouter);
 
-/*
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-*/
+app.use("/api/tasks", tasksRouter);
 
 app.get("*", (req, res) => {
-  // Serve the index.html from the client directory
-  res.sendFile(path.join(__dirname, "..", "..", "client", "public", "index.html"));
+  res.sendFile(path.join(__dirname, '..', '..', 'client', 'public', 'index.html'));
 });
+
 
 const port = process.env.PORT || 10000;
 
-// Bind the server to host 0.0.0.0 and the specified port
-app.listen(port => {
-  console.log(`App listening on ${port}!`);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });
+
 
 module.exports = app;
