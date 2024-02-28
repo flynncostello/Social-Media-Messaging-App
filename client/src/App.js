565883fa928998eb9e981875826b1e45603f4a95
Flynn Costello <flynn.p.costello@outlook.com>
2024-02-28 16:16:15 +1100

    changed routing frontend to persist upon reload

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import Home from './components/home/Home';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import Tasks from './components/tasks/Tasks';
@@ -9,15 +7,40 @@ import TaskPage from './components/tasks/TaskPage';
 import Home from './components/home/Home';
 
 export default function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Save the current route path in localStorage when route changes
+    const handleRouteChange = () => {
+      localStorage.setItem('lastPath', window.location.pathname);
+    };
+
+    const unlisten = navigate(handleRouteChange);
+
+    return () => {
+      unlisten();
+    };
+  }, [navigate]);
+
+  useEffect(() => {
+    // Retrieve the last path from localStorage
+    const lastPath = localStorage.getItem('lastPath');
+
+    // If there's a last path, navigate to it on initial render
+    if (lastPath) {
+      navigate(lastPath);
+    }
+  }, [navigate]);
+
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/tasks" element={<Tasks/>}/>
-          <Route path="/tasks/:task_id" element={<TaskPage/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/tasks/:task_id" element={<TaskPage />} />
         </Routes>
       </Router>
     </Provider>
-  )
-};
+  );
+}
