ebb1134e0da46673cba1843562a260518fdd0db7
Flynn Costello <flynn.p.costello@outlook.com>
2024-03-12 16:48:31 +1100

    template for local full stack app running

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,3 +1,1 @@
-//export const API_ENDPOINT = "http://localhost:5001/api";
-export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
-//https://full-stack-to-do-list-app-backend-server.onrender.com/api
+export const API_ENDPOINT = "http://localhost:3000/api";
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
-//const path = require("path");
+const path = require("path");
 
 const tasksRouter = require("./routes/tasksRouter");
 
@@ -11,29 +13,14 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("https://full-stack-to-do-list-app.onrender.com/api/tasks", tasksRouter);
 app.use("/api/tasks", tasksRouter);
 
+const port = process.env.PORT || 3000;
 
-/*
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'client', 'public', 'index.html'));
-});
-*/
-
-
-//const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 10000;
-
-// Increase server.keepAliveTimeout and server.headersTimeout
 const server = app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
 });
-
-// Set server.keepAliveTimeout and server.headersTimeout to 300 seconds (5 minutes)
-//server.keepAliveTimeout = 300000; // 300 seconds
-//server.headersTimeout = 300000;  // 300 seconds
 
 module.exports = app;
